6f3f84657b0a81008928ace4a7ca8d5d2a81d3f3
Jorge Goris <J_goris@live.com>
2020-08-14 03:36:20 -0400

    reworked how memes are displayed by using an API and saving the urls to state instead of hard coding images in. Then passed those urls down as props to display on the generate meme page.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Navigation from './Components/Navigation';
 import GenerateMeme from './Components/GenerateMeme';
 import GenerateChosenMeme from './Components/GenerateChosenMeme';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navigation />
-        <Switch>
-          <Route path='/generatememe' component={GenerateMeme} />
-          <Route path='/generatechosenmeme' component={GenerateChosenMeme}/>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      allmemeImgs:[]
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://api.imgflip.com/get_memes')
+    .then(res => res.json())
+    .then(res => {
+      const {memes} = res.data
+      this.setState({ allmemeImgs: memes }) 
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Navigation />
+          <Switch>
+            <Route path='/generatememe' render={(props) =>(<GenerateMeme {...props} allmemeImgs={this.state.allmemeImgs} />)}/>
+            <Route path='/generatechosenmeme' component={GenerateChosenMeme}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,32 +1,30 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './Stylesheets/Navigation.css'
 import pepeLogo from '../pepeLogo.png'
 import {Link} from 'react-router-dom'
 
-class Navigation extends Component {
-    render(){
-        return(
-            <div className='navigation__Container'>
-                <div className='navigation__LogoSection'>
+function Navigation(){
+     return(
+        <div className='navigation__Container'>
+            <div className='navigation__LogoSection'>
                     <Link to='/'>
                         <img className='navigation__Logo' src={pepeLogo} alt='Pepe the frog'/>
                         <h4>React Meme Generator</h4>
                     </Link>
                 </div>
-                <div className='navigation__SearchSection'>
+            <div className='navigation__SearchSection'>
                     <input type='text' placeholder='Search Memes..' />
                 </div>
-                <div className='navigation__ButtonsSection'>
-                    <Link to='/generatememe'><span role='img' aria-label='pencil'>✏️ </span>Generate</Link>
-                    <a href='#'><span role='img' aria-label='star'>⭐ </span>Top Memes</a>
-                </div>
-                <div className='navigation__LoginSection'>
+            <div className='navigation__ButtonsSection'>
+                <Link to='/generatememe'><span role='img' aria-label='pencil'>✏️ </span>Generate</Link>
+                <a href='#'><span role='img' aria-label='star'>⭐ </span>Top Memes</a>
+            </div>
+            <div className='navigation__LoginSection'>
                     <a href='#'>Login</a>
                     <a href='#'>Sign Up</a>
                 </div>
-            </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Navigation
